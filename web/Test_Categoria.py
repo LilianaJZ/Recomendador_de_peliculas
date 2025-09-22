d10008376e1830c6a26ba1636737bb853e5ea5ae
LilianaJZ <ljimenezz@unal.edu.co>
2025-09-22 14:52:56 -0500

    test: añadir clase TestCategoria para probar creación y listado de categorías

--- web/Test_Categoria.py
+++ web/Test_Categoria.py
@@ -1,8 +1,12 @@
 from domain.model.Categoria import Categoria
-from
+from domain.service.Categoria_Service import Categoria_Service
+from persistence.Categoria_Repositorio import Categoria_Repositorio
+
 
 class TestCategoria:
-    categoria = Categoria(id_categoria: None, categoria None)
+
+    categoria = Categoria(None, None)
     categoria_service = Categoria_Service()
 
     categoria_service.crear_categoria()
+    categoria_service.mostrar_categorias()
