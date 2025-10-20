2f6492b568e0d974df92083e2b9957235ae62827
LilianaJZ <ljimenezz@unal.edu.co>
2025-10-19 22:50:48 -0500

    Creación inicial de Pelicula_Repositorio con conexión a la base de datos

--- persistence/Pelicula_Repositorio.py
+++ persistence/Pelicula_Repositorio.py
@@ -1,11 +1,11 @@
-from persistence.Conexion import conexion
+from persistence.Conexion import Conexion
 
 class Pelicula_Repositorio:
 
     def __init__(self):
-        self.db = conexion ( host='localhost', port=3306, uer='root', password="", database='movie_recomender_DB' )
+        self.db = Conexion ( host='localhost', port=3306, user='root', password="", database='movie_recommender_DB' )
         self.db.connect()
 
 
-    def crear_pelicula(self):
-        query = """INSERT INTO """
+    def crear_pelicula_DB(self):
+        query = "INSERT INTO "
