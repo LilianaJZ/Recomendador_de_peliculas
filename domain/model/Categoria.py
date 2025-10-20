c0dcb83e738f4a33e35a5456af0504547544ae20
LilianaJZ <ljimenezz@unal.edu.co>
2025-10-19 22:36:29 -0500

    refactor: cambia nombres de atributos por claridad y agrega comentarios en Categoria

--- domain/model/Categoria.py
+++ domain/model/Categoria.py
@@ -1,16 +1,16 @@
 class Categoria:
     def __init__(self, id_categoria, categoria):
-        self._id_categoria = id_categoria
-        self._categoria = categoria
+        self.__id_categoria = id_categoria
+        self.__categoria = categoria
 
     # --- Propiedad para 'id_categoria' ---
     @property
     def id_categoria(self):
-        return self._id_categoria
+        return self.__id_categoria
 
     @id_categoria.setter
     def id_categoria(self, id_categoria):
-        self._id_categoria = id_categoria
+        self.__id_categoria = id_categoria
 
     # --- Propiedad para 'categoria' ---
     @property
@@ -25,8 +25,9 @@ class Categoria:
     def from_row(row):
         return Categoria(row[0], row[1])
 
+#Metodo que nos convierte en imprimible cualquier valor que venga
     def __str__(self):
         return f"Categoria(id_categoria ={self.id_categoria}, nombre_categoria={self.categoria})"
-
+#Metodo que organiza lo que le devuelva
     def __repr__(self):
         return self.__str__()
