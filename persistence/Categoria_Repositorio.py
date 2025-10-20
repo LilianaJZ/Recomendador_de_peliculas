0149fe8a6fdc7002f16b72079ca4255317e5b6c5
LilianaJZ <ljimenezz@unal.edu.co>
2025-10-19 22:48:31 -0500

    Creación de Categoria_Repositorio con métodos CRUD para la base de datos

--- persistence/Categoria_Repositorio.py
+++ persistence/Categoria_Repositorio.py
@@ -21,9 +21,28 @@ class Categoria_Repositorio:
         if result:
             categoria_lista = []
             for row in result:
-                categoria  = Categoria.from_row(row)
+                categoria = Categoria.from_row(row)
                 categoria_lista.append(categoria)
             return categoria_lista
         else:
             print("No se ha podido recuperar categorias")
             return []
+
+
+    def listar_categoria_por_id(self, categoria_id):
+        query = "Select * from categoria where id_categoria = %s"
+        result = self.db.execute_query(query, (categoria_id,))
+        if result:
+            return Categoria.from_row(result[0])
+        else:
+            print("registro no encontrado")
+            return []
+
+    def actualizar_categoria(self, categoria_id, categoria):
+        query = "UPDATE categoria set nombre_categoria = %s where id_categoria = %s "
+        values = (categoria.categoria,categoria_id)
+        self.db.execute_query(query, values)
+
+    def eliminar_categoria(self, categoria_id):
+        query = "DELETE FROM categoria where id_categoria = %s"
+        self.db.execute_query(query, (categoria_id,))
