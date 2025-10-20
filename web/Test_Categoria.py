cd3ee35996705af7f4b98a15c2aee7ae76631dbc
LilianaJZ <ljimenezz@unal.edu.co>
2025-10-19 22:56:49 -0500

    Creación del archivo de prueba TestCategoria con métodos de prueba comentados

--- web/Test_Categoria.py
+++ web/Test_Categoria.py
@@ -3,10 +3,14 @@ from domain.service.Categoria_Service import Categoria_Service
 from persistence.Categoria_Repositorio import Categoria_Repositorio
 
 
-class TestCategoria:
+#class TestCategoria:
 
-    categoria = Categoria(None, None)
-    categoria_service = Categoria_Service()
 
-    categoria_service.crear_categoria()
-    categoria_service.mostrar_categorias()
+    #categoria = Categoria(None, None)
+   # categoria_service = Categoria_Service()
+
+    #categoria_service.crear_categoria()
+    #categoria_service.mostrar_categorias()
+    #categoria_service.mostrar_categoria_por_id()
+    #categoria_service.actualizar_categoria_por_id()
+    #categoria_service.borrar_categoria()
