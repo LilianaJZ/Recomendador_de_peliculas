0b49e66a0065ff95fdeb039464badbc53e9a62fd
LilianaJZ <ljimenezz@unal.edu.co>
2025-10-19 23:00:09 -0500

    Creación del archivo de prueba Test_Pelicula con invocación de crear_pelicula

--- web/Test_Pelicula.py
+++ web/Test_Pelicula.py
@@ -1,13 +1,13 @@
 from domain.model.Categoria import Categoria
 from domain.model.Pelicula import Pelicula
-from domain.service.Pelicula_Service import PeliculaService
+from domain.service.Pelicula_Service import Pelicula_Service
 
 
 class Test_Pelicula:
 
     categoria = Categoria(None,None)
     pelicula = Pelicula(None,None,None,None,None,None,None,None,None)
-    pelicula_service = PeliculaService()
+    pelicula_service = Pelicula_Service()
 
 
 
